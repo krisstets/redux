3be67f15763303cd143f10d3254a2f8ea2f1dd04
DellVostro253 <krisstets>
2020-02-27 18:01:11 +0300

    feat: add store for posts

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import './App.css';
-import  PostsList  from "./Components/PostsList";
+import  PostsList  from "./Components/postsList";
 import PostInfo from './Components/postInfo';
 import {BrowserRouter as Router, Switch, Route} from 'react-router-dom';
 import Nav from './Components/Nav';
